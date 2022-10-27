1315032ef20037898d7a16332690796c3492e435
exaltedr <109990032+exaltedr@users.noreply.github.com>
2022-10-27 18:05:38 -0400

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,115 @@
+// questions + answers array
+let questions = [
+    {
+    numb: 1,
+    question: "Inside which HTML element do we put the Javascript?",
+    answer: "script",
+    options: [
+      "script",
+      "javascript",
+      "js",
+      "scripting"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: "Both the header section and the body section are correct.",
+    options: [
+      "Both the header section and the body section are correct.",
+      "The body section",
+      "The header section",
+      "The footer section"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How to write an IF statement in JavaScript?",
+    answer: "if (i == 5)",
+    options: [
+      "if i == 5 then",
+      "if (i == 5)",
+      "if i = 5",
+      "if i = 5 then"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How can you add a comment in a JavaScript?",
+    answer: "//This is a comment//",
+    options: [
+      "--This is a comment--",
+      "!This is a comment!",
+      "'This is a comment",
+      "//This is a comment//"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "onclick",
+    options: [
+      "onmouseover",
+      "onclick",
+      "onmouseclick",
+      "onchange"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Which of the following is an advantage of using JavaScript?",
+    answer: "All of the above.",
+    options: [
+      "Less server interaction",
+      "Immediate feedback to the visitors",
+      "Increased interactivity",
+      "All of the above."
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which of the following is true about cookie handling in JavaScript?",
+    answer: "Both of the above.",
+    options: [
+      "JavaScript can manipulate cookies using the cookie property of the Document object.",
+      "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+      "Both of the above.",
+      "None of the above."
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which of the following type of variable is visible only within a function where it is defined?",
+    answer: "local variable",
+    options: [
+      "global variable",
+      "local variable",
+      "Both of the above.",
+      "None of the above."
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which of the following code creates an object?",
+    answer: "var book = new Object();",
+    options: [
+      "var book = Object();",
+      "var book = new Object();",
+      "var book = new OBJECT();",
+      "var book = new Book();"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which of the following function of Number object returns the number's value?",
+    answer: "valueOf()",
+    options: [
+      "toString()",
+      "valueOf()",
+      "toLocaleString()",
+      "toPrecision()"
+    ]
+  },
+  
+ 
+];
